cc8761e1e4ca3e6ef333890eda7b1b8aca441c9e
Kaydayo <olukayodedayo100@gmail.com>
2024-06-18 07:07:58 +0100

    created auth module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
+const logger: Logger = new Logger('Main');
+const port = new ConfigService().get('PORT');
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port, () => {
+    logger.log('---------------------------------');
+    logger.log(`🚀 App is listening on ${port} 🚀`);
+    logger.log('---------------------------------');
+  });
 }
 bootstrap();
