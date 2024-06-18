87bde9d22455b89bb4f6076cad5e3bf114b74e5f
Kaydayo <olukayodedayo100@gmail.com>
2024-06-18 10:55:41 +0100

    create facebook login route

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import { FacebookStrategy } from './common/strategy/facebook.strategy';
 
 @Module({
   imports: [ConfigModule.forRoot(), AuthModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, FacebookStrategy],
 })
 export class AppModule {}
