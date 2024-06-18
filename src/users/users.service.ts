a1a4d34ce9c772db6fd3b6a25b898575203180b1
Kaydayo <olukayodedayo100@gmail.com>
2024-06-18 20:37:06 +0100

    setup class service for user module

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { Supabase } from '../common/supabase';
+import { SupabaseClient } from '@supabase/supabase-js';
 
 @Injectable()
 export class UsersService {
+    private supabaseClient:SupabaseClient
     constructor(private supabase: Supabase) {
-        
+        this.supabaseClient = this.supabase.getClient()
+    }
+
+    async create(data:any) {
+        const { data: result, error } = await this.supabaseClient.from('users').insert(data)
+        if (error) throw new BadRequestException(error.message)
+        return result
+    }
+
+    async getOne(query:any) {
+        const { data: result, error } = await this.supabaseClient.from('users').select().match(query)
+        if (error) throw new BadRequestException(error.message)
+        return result
+    }
+
+    async getAll() {
+        const { data: result, error } = await this.supabaseClient.from('users').select()
+        if (error) throw new BadRequestException(error.message)
+        return result
+    }
+
+    async update(query:any, data:any) {
+        const { data: result, error } = await this.supabaseClient
+            .from('users')
+            .update(data)
+            .match(query);
+        if (error) throw new BadRequestException(error.message);
+        return result;
+    }
+
+    async delete(table: string, query: any) {
+        const { data: result, error } = await this.supabaseClient
+            .from(table)
+            .delete()
+            .match(query);
+        if (error) throw new BadRequestException(error.message);
+        return result;
     }
 }
